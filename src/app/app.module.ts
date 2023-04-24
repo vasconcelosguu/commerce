cc32b7ec41e6b33532827bf2695aa45de7406d07
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-24 15:07:15 -0300

    :art: Adding Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,12 @@ import {MatListModule} from '@angular/material/list'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
