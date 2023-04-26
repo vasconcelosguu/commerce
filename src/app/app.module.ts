11188935a6cf245474ed71e507525759e1ba49ee
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-26 15:18:34 -0300

    :wrench: files locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
-import { FiltersComponent } from './components/filters/filters.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { FiltersComponent } from './components/filters/filters.component';
     HeaderComponent,
     HomeComponent,
     ProductsHeaderComponent,
-    FiltersComponent
+    FiltersComponent,
+    ProductBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-box',
+  template: `
+    <p>
+      product-box works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProductBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
