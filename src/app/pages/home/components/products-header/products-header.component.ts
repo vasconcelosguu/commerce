c39d8997d25053468b1119cc2127504c5e340409
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-26 14:21:25 -0300

    :zap: adding home and home-header

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html'
 })
-export class ProductsHeaderComponent implements OnInit {
+export class ProductsHeaderComponent 
+
+implements OnInit {
+
+  @Output() columnsCountCHange = new EventEmitter<number>()
+  sort = 'desc';
+  itemCount = 12;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sortHandler(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  itemsHandler(itemCount: number): void{
+    this.itemCount = itemCount;
+  }
+
+  columnsUpdater(cols: number): void{
+    this.columnsCountCHange.emit(cols)
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,14 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
 })
 export class HomeComponent implements OnInit {
+  cols = 3;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  columnsUpdater(val: any): void {
+    this.cols = val;
   }
 
 }
