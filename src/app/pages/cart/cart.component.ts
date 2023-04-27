7a6f2e296500c8f750239bf5d43bc00e87c68551
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-27 15:38:47 -0300

    :sparkles: Cart interface and tables of cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,14 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/interfaces/cart.interface';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
 })
 export class CartComponent implements OnInit {
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 2,
+  }
+]};
 
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ]
+ 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map(({price, quantity}) => price * quantity).reduce((acc, acr) => acc + acr, 0)
   }
 
 }
