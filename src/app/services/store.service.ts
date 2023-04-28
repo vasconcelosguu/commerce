a1340536333995d21dbf9cf48ae1827bf420c331
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-28 18:38:41 -0300

    :package: service categories

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -12,9 +12,18 @@ export class StoreService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAllProducts(limit= '12', sort= 'desc'): Observable<Array<Product>> {
+  getAllProducts(limit= '12', sort= 'desc', category?: string): Observable<Array<Product>> {
     return this.httpClient.get<Array<Product>>(
-      `${URL}/products?sort=${sort}&limit=${limit}`
+      `${URL}/products${
+        category ? '/category/' + category: ''
+      }?sort=${sort}&limit=${limit}`
     )
   }
+
+  getAllCategories(): Observable<Array<string>> {
+    return this.httpClient.get<Array<string>>(
+     `${URL}/products/categories` 
+    )
+  }
+
 }
